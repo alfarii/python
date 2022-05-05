8c3f248cd927d7389d8cca7fc2ee5d77486a7005
alfarii <104956144+alfarii@users.noreply.github.com>
2022-05-05 09:40:32 +0700

    Add files via upload

---
+++ 1111.py
@@ -0,0 +1,9 @@
+login = input("Введите логин:")
+parol=input("Введите пароль:")
+while login != "ll" and parol !="2":
+     if login !="ll":
+         print("Логин не верен")
+     if parol !="2": 
+         print('аоль не верен')
+     if login  and parol !=    
+print ("Добро пжаловать!")
