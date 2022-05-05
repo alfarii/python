ff3d43652ca8adee1e5213de417b76199d26e7a9
alfarii <104956144+alfarii@users.noreply.github.com>
2022-05-05 09:26:30 +0700

    Add files via upload

---
+++ виля.py
@@ -0,0 +1,7 @@
+login = input("Введите логин:")
+parol=input("Введите пароль:")
+while login != "ll" and parol !="2":
+      print("Логин или парольне верен.")
+      login=input('Введите логин')
+      parol=input('Введите пароль')
+print ("Добро пжаловать!")
